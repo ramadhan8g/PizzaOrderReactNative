5849cc542de8162bfa93846ef9f919711022fe11
ramadhan8g <ramadhanyandra13@gmail.com>
2024-03-28 14:01:02 +0700

    add flatlist,type

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
-import { View } from 'react-native';
-import products from '../../../assets/data/products';
-import ProductListItem from '../../components/ProductListItem';
+import { FlatList } from 'react-native';
+import products from '@assets/data/products';
+import ProductListItem from '@components/ProductListItem';
 
 export default function MenuScreen() {
   return (
-    <View>
-      <ProductListItem product={products[5]} />
-      <ProductListItem product={products[1]} />
-    </View>
+    // ada fitur gulirnya
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+      numColumns={2}
+      contentContainerStyle={{ gap: 10, padding: 10 }}
+      columnWrapperStyle={{ gap: 10 }}
+    />
   );
 }
